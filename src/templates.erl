ariel-anieli/flop

commit 088683a49b46f78bcead5e83980c2f131e4a934d

Clear out warnings about shadowed variables
Signed-off-by: Ariel <otilibil@eurecom.fr>

src/templates.erl
@@ -9,7 +9,7 @@
 ]).
 
 pipe(Arg, Funcs) -> 
-    lists:foldl(fun(Func, Arg) -> Func(Arg) end, Arg, Funcs).
+    lists:foldl(fun(Func, Acc) -> Func(Acc) end, Arg, Funcs).
 
 get_template(#{type:=nxos, request:=description}) ->
     "cx=!name!;to=!to-dev!_!to-port!";
@@ -120,8 +120,8 @@ get_key(#{key:='to port', link := #{to:=To}}) ->
 get_key(#{key:='to aggr port', link:=Link, db := #{links:=Links}}) -> 
     GetPort = fun(#{to := #{port:=Port}}) -> Port end,
     Aggr    = maps:get(aggr, Link, 0),
-    Ports   = [integer_to_list(GetPort(Link))
-	       || Link <- Links, Aggr=:=maps:get(aggr,Link,0)],
+    Ports   = [integer_to_list(GetPort(L))
+	       || L <- Links, Aggr=:=maps:get(aggr,Link,0)],
 
     #{
       key => "!to-aggr-port!",
@@ -151,8 +151,8 @@ get_key(#{key:=vlan, link := #{vlan:=VLAN}}) ->
 get_key(#{key:='vlans from aggr', db := #{links:=Links}, link:=Link}) ->
     Aggr  = maps:get(aggr, Link, 0),
     VLANs = pipe(
-	      [list_vlans(maps:get(vlan, Link)) 
-	       || Link <- Links, Aggr=:=maps:get(aggr,Link,0)],
+	      [list_vlans(maps:get(vlan, L))
+	       || L <- Links, Aggr=:=maps:get(aggr,Link,0)],
 	      [
 	       fun lists:merge/1,
 	       fun lists:sort/1,
@@ -212,7 +212,7 @@ merge_snippets_if_needed(#{request:=Request})
 
 build_snippet_using_keys(#{db := #{links:=Links}, request:=_Request} = Args) ->
     BuildFromLink = fun(Link)  -> build_from_link(Args#{link=>Link}) end,
-    BuildAllLinks = fun(Links) -> lists:map(BuildFromLink, Links) end,
+    BuildAllLinks = fun(Lks) -> lists:map(BuildFromLink, Lks) end,
     MergeIfNeeded = merge_snippets_if_needed(Args),
     pipe(Links,
 	 [

src/contract_checker.erl
@@ -6,7 +6,7 @@
 -define(HEAD_IS_UPPC(Str), hd(Str)>=65,hd(Str)=<90).
 -define(HEAD_IS_LOWC(Str), hd(Str)>=97,hd(Str)=<122).
 
--export([init/1, handle_call/3]).
+-export([init/1, handle_call/3, handle_cast/2]).
 
 -export([
 	 start_link/0,
@@ -38,6 +38,9 @@ handle_call(#{req:=fault, type:=Type}, _From, State) ->
     {reply, Fault, State};
 handle_call(#{req:=stop}, _From, DB) ->
     {stop, normal, stopped, DB}.
+
+handle_cast(_, DB) ->
+    {noreply, DB}.
 
 is_valid(Term) when is_boolean(Term) -> 
     Term==true;

src/helpers.erl
@@ -25,7 +25,7 @@
 ]).
 
 pipe(Arg, Funcs) -> 
-    lists:foldl(fun(Func, Arg) -> Func(Arg) end, Arg, Funcs).
+    lists:foldl(fun(Func, Acc) -> Func(Acc) end, Arg, Funcs).
 
 open_db_or_create_from_template({ok, [DB]}, _) ->
     Links       = maps:get(links, DB),

commit 67e187b3bc69e58026ba19b4f20688b025d9f71f

Fix a regression on `intf_prt_chnl/1`
Introduced by 088683a49b46f78bcead5e83980c2f131e4a934d.

Fixes: #34
Signed-off-by: Ariel <otilibil@eurecom.fr>

src/templates.erl
@@ -120,8 +120,8 @@ get_key(#{key:='to port', link := #{to:=To}}) ->
 get_key(#{key:='to aggr port', link:=Link, db := #{links:=Links}}) -> 
     GetPort = fun(#{to := #{port:=Port}}) -> Port end,
     Aggr    = maps:get(aggr, Link, 0),
-    Ports   = [integer_to_list(GetPort(L))
-	       || L <- Links, Aggr=:=maps:get(aggr,Link,0)],
+    Ports   = [integer_to_list(GetPort(Link))
+	       || Link <- Links, Aggr=:=maps:get(aggr,Link,0)],
 
     #{
       key => "!to-aggr-port!",
@@ -151,8 +151,8 @@ get_key(#{key:=vlan, link := #{vlan:=VLAN}}) ->
 get_key(#{key:='vlans from aggr', db := #{links:=Links}, link:=Link}) ->
     Aggr  = maps:get(aggr, Link, 0),
     VLANs = pipe(
-	      [list_vlans(maps:get(vlan, L))
-	       || L <- Links, Aggr=:=maps:get(aggr,Link,0)],
+	      [list_vlans(maps:get(vlan, Link))
+	       || Link <- Links, Aggr=:=maps:get(aggr,Link,0)],
 	      [
 	       fun lists:merge/1,
 	       fun lists:sort/1,